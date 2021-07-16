52ec45e0ff0a479d435d45ec2794c93af3574c99
刘斌 <liu_bin4835@outlook.com>
2021-07-16 09:12:02 +0800

    ...

--- hdfs/src/main/java/com/liubin/hadoop/hdfs/HDFSClient.java
+++ hdfs/src/main/java/com/liubin/hadoop/hdfs/HDFSClient.java
@@ -29,93 +29,93 @@ import java.net.URISyntaxException;
  */
 public class HDFSClient {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(HDFSClient.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HDFSClient.class);
 
-  private static FileSystem fileSystem;
+    private static FileSystem fileSystem;
 
-  @BeforeAll
-  static void beforeAll() throws URISyntaxException, IOException, InterruptedException {
-    final Configuration configuration = new Configuration();
-    configuration.set("dfs.replication", "1");
-    fileSystem = FileSystem.get(new URI("hdfs://localhost:9000"), configuration, "liubin");
-    LOGGER.info("开启链接");
-  }
-
-  @AfterAll
-  static void afterAll() throws IOException {
-    fileSystem.close();
-    LOGGER.info("关闭链接");
-  }
-
-  /**
-   * 文件上传
-   *
-   * @throws IOException IOException
-   */
-  @Test
-  public void copyFromLocalFile() throws IOException {
-    fileSystem.copyFromLocalFile(
-        new Path("D:/data/100000_full.json"), new Path("/users/liubin/input/2021-03-29"));
-  }
-
-  /**
-   * 文件上传 io
-   *
-   * @throws IOException IOException
-   */
-  @Test
-  public void creat() throws IOException {
-    try (FileInputStream fileInputStream =
-            new FileInputStream(new File("D:/data/100000_full.json"));
-        final FSDataOutputStream fsDataOutputStream =
-            fileSystem.create(new Path("/users/liubin/input/2021-03-30"))) {
-      IOUtils.copyBytes(fileInputStream, fsDataOutputStream, 1024);
+    @BeforeAll
+    static void beforeAll() throws URISyntaxException, IOException, InterruptedException {
+        final Configuration configuration = new Configuration();
+        configuration.set("dfs.replication", "1");
+        fileSystem = FileSystem.get(new URI("hdfs://localhost:9000"), configuration, "liubin");
+        LOGGER.info("开启链接");
     }
-  }
 
-  /**
-   * 文件下载
-   *
-   * @throws IOException IOException
-   */
-  @Test
-  void copyToLocalFile() throws IOException {
-    fileSystem.copyToLocalFile(
-        false, new Path("/users/liubin/output"), new Path("D:/data/output"), true);
-  }
-
-  @Test
-  void listFiles() throws IOException {
-    final RemoteIterator<LocatedFileStatus> listFiles =
-        fileSystem.listFiles(new Path("/users"), true);
-
-    while (listFiles.hasNext()) {
-      final LocatedFileStatus next = listFiles.next();
-      LOGGER.info(
-          "f:{},length:{}kb,permission:{}",
-          next.getPath().getName(),
-          next.getLen() / 1024,
-          next.getPermission());
+    @AfterAll
+    static void afterAll() throws IOException {
+        fileSystem.close();
+        LOGGER.info("关闭链接");
     }
-  }
 
-  @Test
-  void listStatus() throws IOException {
-    final FileStatus[] fileStatuses = fileSystem.listStatus(new Path("/users"));
-    for (FileStatus fileStatus : fileStatuses) {
-      if (fileStatus.isFile()) {
-        LOGGER.info(
-            "f:{},length:{}kb,permission:{}",
-            fileStatus.getPath().getName(),
-            fileStatus.getLen() / 1024,
-            fileStatus.getPermission());
-      } else {
-        LOGGER.info(
-            "d:{},length:{}kb,permission:{}",
-            fileStatus.getPath().getName(),
-            fileStatus.getLen() / 1024,
-            fileStatus.getPermission());
-      }
+    /**
+     * 文件上传
+     *
+     * @throws IOException IOException
+     */
+    @Test
+    public void copyFromLocalFile() throws IOException {
+        fileSystem.copyFromLocalFile(
+                new Path("D:/data/100000_full.json"), new Path("/users/liubin/input/2021-03-29"));
     }
-  }
+
+    /**
+     * 文件上传 io
+     *
+     * @throws IOException IOException
+     */
+    @Test
+    public void creat() throws IOException {
+        try (FileInputStream fileInputStream =
+                     new FileInputStream(new File("D:/data/100000_full.json"));
+             final FSDataOutputStream fsDataOutputStream =
+                     fileSystem.create(new Path("/users/liubin/input/2021-03-30"))) {
+            IOUtils.copyBytes(fileInputStream, fsDataOutputStream, 1024);
+        }
+    }
+
+    /**
+     * 文件下载
+     *
+     * @throws IOException IOException
+     */
+    @Test
+    void copyToLocalFile() throws IOException {
+        fileSystem.copyToLocalFile(
+                false, new Path("/users/liubin/output"), new Path("D:/data/output"), true);
+    }
+
+    @Test
+    void listFiles() throws IOException {
+        final RemoteIterator<LocatedFileStatus> listFiles =
+                fileSystem.listFiles(new Path("/users"), true);
+
+        while (listFiles.hasNext()) {
+            final LocatedFileStatus next = listFiles.next();
+            LOGGER.info(
+                    "f:{},length:{}kb,permission:{}",
+                    next.getPath().getName(),
+                    next.getLen() / 1024,
+                    next.getPermission());
+        }
+    }
+
+    @Test
+    void listStatus() throws IOException {
+        final FileStatus[] fileStatuses = fileSystem.listStatus(new Path("/users"));
+        for (FileStatus fileStatus : fileStatuses) {
+            if (fileStatus.isFile()) {
+                LOGGER.info(
+                        "f:{},length:{}kb,permission:{}",
+                        fileStatus.getPath().getName(),
+                        fileStatus.getLen() / 1024,
+                        fileStatus.getPermission());
+            } else {
+                LOGGER.info(
+                        "d:{},length:{}kb,permission:{}",
+                        fileStatus.getPath().getName(),
+                        fileStatus.getLen() / 1024,
+                        fileStatus.getPermission());
+            }
+        }
+    }
 }
